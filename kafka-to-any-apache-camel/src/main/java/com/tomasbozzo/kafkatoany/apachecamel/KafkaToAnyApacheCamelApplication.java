6e56c317509f78218dcfa31e5702b4b7c949a1db
tomasbozzo <tomasbozzo@gmail.com>
2022-01-12 10:47:32 +0100

    Initial commit

---
+++ kafka-to-any-apache-camel/src/main/java/com/tomasbozzo/kafkatoany/apachecamel/KafkaToAnyApacheCamelApplication.java
@@ -0,0 +1,13 @@
+package com.tomasbozzo.kafkatoany.apachecamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaToAnyApacheCamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToAnyApacheCamelApplication.class, args);
+    }
+
+}
---
+++ kafka-to-any-apache-camel/src/test/java/com/tomasbozzo/kafkatoany/apachecamel/KafkaToAnyApacheCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomasbozzo.kafkatoany.apachecamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaToAnyApacheCamelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
